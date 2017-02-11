7d2e1ae70b3f86a8093751702f3b61b6d32c68b3
MauriDV <mdellevedove12@gmail.com>
2017-02-11 01:44:02 -0300

    basic server configurations

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname,'..','client'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'..','/public')));
+
+app.get('*',(req,res)=>{
+    res.render('index')
+});
+
+module.exports = app;
